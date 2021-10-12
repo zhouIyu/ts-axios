19256cc84ec3b5088dd2cb1ed0e2441d11b82af6
zhouzhuang <zhouzhuang@tal.com>
2021-10-12 14:48:01 +0800

    feat: 🎸 20.HTTP授权

--- src/type.ts
+++ src/type.ts
@@ -30,8 +30,14 @@ export interface AxiosRequestConfig {
   xsrfHeaderName?: string
   onDownloadProgress?: (e: ProgressEvent) => void
   onUploadProgress?: (e: ProgressEvent) => void
+  auth?: AxiosBaseCredentials
 
   [propName: string]: any
+}
+
+export interface AxiosBaseCredentials {
+  username: string
+  password: string
 }
 
 export interface AxiosTransformer {
